9f6f6cabdd0ba201287c195c78f7676c9b2aea57
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-11-15 09:37:28 +0100

    5

--- Ejercicio05.py
+++ Ejercicio05.py
@@ -5,7 +5,9 @@ def area_cuadrado(lado):
     return Area
 lado = int(input('Introduce un lado'))
 area_cuadrado(lado)
-
+'''Hemos realizado una funcion para realizar el area de un cuadrado
+en este caso le pedimos al usuario un lado del cuadrado y dentro de la funcion 
+realizamos la operación'''
 ###Mayor de 3
 def mayor_de_tres(a, b, c):
     print (max(a,b,c))
@@ -13,3 +15,5 @@ a = int(input('Intrduce tu primer numero'))
 b = int(input('Intrduce tu primer numero'))
 c = int(input('Intrduce tu primer numero'))
 mayor_de_tres(a,b,c)
+'''En este caso realizamos una funcion que recoja 3 variables
+para comparar cual es el mayor'''
